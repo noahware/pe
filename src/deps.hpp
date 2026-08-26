975ad1000cccb3e6bcec5cbc5f225bb457f577d0
noahware <noahswtf@icloud.com>
2026-08-26 11:36:23 +0100

    Started exports directory parsing

--- src/deps.hpp
+++ src/deps.hpp
@@ -1,8 +1,12 @@
 #pragma once
-#include <span>
 #include <string_view>
+#include <optional>
+#include <span>
 
 template <class T>
 using span_t = std::span<T>;
+
+template <class T>
+using optional_t = std::optional<T>;
 
 using string_view_t = std::string_view;
---
+++ src/export_directory.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <cstdint>
+#include "deps.hpp"
+#include "addr.hpp"
+
+namespace pe
+{
+	struct export_info
+	{
+		bool is_ordinal;
+		std::uint32_t ordinal;
+		string_view_t name;
+		bin_addr loc;
+	};
+
+	struct export_directory
+	{
+		std::uint32_t characteristics;
+		std::uint32_t time_date_stamp;
+		std::uint16_t major_version;
+		std::uint16_t minor_version;
+		std::uint32_t name;
+		std::uint32_t base;
+		std::uint32_t number_of_functions;
+		std::uint32_t number_of_names;
+		std::uint32_t address_of_functions;
+		std::uint32_t address_of_names;
+		std::uint32_t address_of_name_ordinals;
+	};
+
+	static_assert(sizeof(export_directory) == 0x28);
+}
