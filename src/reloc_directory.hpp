05f89940a78545ad3aea26101b228c263c5e2ffd
noahware <noahswtf@icloud.com>
2026-08-26 13:51:31 +0100

    Added relocation descriptors

---
+++ src/reloc_directory.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include "deps.hpp"
+
+namespace pe
+{
+	enum class reloc_type : std::uint16_t
+	{
+		absolute = 0,
+		high = 1,
+		low = 2,
+		highlow = 3,
+		highadj = 4,
+		machine_specific_5 = 5,
+		reserved = 6,
+		machine_specific_7 = 7,
+		machine_specific_8 = 8,
+		machine_specific_9 = 9,
+		dir64 = 10
+	};
+
+	union reloc_entry
+	{
+		std::uint16_t raw;
+
+		struct
+		{
+			std::uint16_t offset : 12;
+			std::uint16_t type : 4;
+		};
+	};
+
+	static_assert(sizeof(reloc_entry) == 0x2);
+
+	struct reloc_directory
+	{
+		std::uint32_t virtual_address;
+		std::uint32_t size_of_block;
+	};
+
+	static_assert(sizeof(reloc_directory) == 0x8);
+}
